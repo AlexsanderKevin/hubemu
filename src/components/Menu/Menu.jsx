aac0b0ce7fe98e08bf5237617d73719b2242a52e
ChesterKing66 <kevin13348@gmail.com>
2024-04-05 21:07:59 -0300

    added phosphor icons and styled the menu a little more

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,20 @@
+import { House } from '@phosphor-icons/react/dist/ssr'
 import styles from './Menu.module.css'
+import { List, Play } from '@phosphor-icons/react'
 
 export default function Menu() {
   return (
     <aside className={styles.menu}>
-      Menu
-      <button>home</button>
-      <button>home</button>
-      <button>home</button>
-      <button>home</button>
-      <button>home</button>
+      <h1 className={styles.title}><List/> Menu</h1>
+
+      <div className={styles.linkGroup}>
+        <button className={styles.navLink}><House weight='bold' /> Home</button>
+        <button className={styles.navLink}><House weight='bold' /> Home</button>
+        <button className={styles.navLink}><House weight='bold' /> Home</button>
+        <button className={styles.navLink}><House weight='bold' /> Home</button>
+      </div>
+
+      <h1 className={styles.title}><Play/> Inicio Rapido</h1>
     </aside>
   )
 }
