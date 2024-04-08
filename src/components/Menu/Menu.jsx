10c95cc403b378a7280c5940e10c876674dbcc4a
ChesterKing66 <kevin13348@gmail.com>
2024-04-08 20:08:32 -0300

    styled more of the menu and added the linear gradient

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { House } from '@phosphor-icons/react/dist/ssr'
+import { House, GearSix, SquaresFour } from '@phosphor-icons/react/dist/ssr'
 import styles from './Menu.module.css'
 import { List, Play } from '@phosphor-icons/react'
 
@@ -7,11 +7,10 @@ export default function Menu() {
     <aside className={styles.menu}>
       <h1 className={styles.title}><List/> Menu</h1>
 
-      <div className={styles.linkGroup}>
-        <button className={styles.navLink}><House weight='bold' /> Home</button>
-        <button className={styles.navLink}><House weight='bold' /> Home</button>
-        <button className={styles.navLink}><House weight='bold' /> Home</button>
-        <button className={styles.navLink}><House weight='bold' /> Home</button>
+      <div className={`${styles.linkGroup} navigation-container vertical`} data-navigation-index="10" data-orientation="vertical">
+        <button className={`${styles.navLink} active navigation-item`}><House weight='bold' /> Home</button>
+        <button className={styles.navLink}><SquaresFour weight='bold' /> Biblioteca</button>
+        <button className={styles.navLink}><GearSix weight='bold' /> Configurações</button>
       </div>
 
       <h1 className={styles.title}><Play/> Inicio Rapido</h1>
